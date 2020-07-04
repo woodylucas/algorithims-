882b80538111c3080c601403c5d23f61b33d0cae
woodylucas <wlucas0311@gmail.com>
2020-07-04 12:48:09 -0400

    recursion

---
+++ app.js
@@ -0,0 +1,10 @@
+// Recrusion 
+// factorial interatively(non recursive solution) 4! ===  4 * 3 * 2 * 1 
+function factorial(num {
+    let total = 1;
+    for(let i = num; i > 0; i--) {
+        total *= i;
+    }
+}
+
+
