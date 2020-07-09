7cba87b72138bd50203f3b075ee3a0374d15d03f
woodylucas <wlucas0311@gmail.com>
2020-07-09 17:03:38 -0400

    grasping closures

---
+++ closure.js
@@ -0,0 +1,36 @@
+function once(cb, output = []) {
+    function insideFunc(...args) {
+      output.push(cb(...args))
+      return output[0]
+    }
+    return insideFunc
+  }
+  
+  const addByTwoOnce = once(function(num) {
+    return num + 2;
+  });
+  
+  // UNCOMMENT THESE TO TEST YOUR WORK!
+   console.log(addByTwoOnce(5));  //should log 7
+   console.log(addByTwoOnce(10));  //should log 7
+   console.log(addByTwoOnce(9001));  //should log 7
+  
+  // PSUEDOCODE 
+  
+  // DECLARE a function: once 
+  
+  // INPUT: cb
+  
+  // DECLARE a variable output w/ an empty array 
+  
+          // RETURN a function declaration: insideFunc
+          
+          // INPUT: rest parameter ...args 
+
+          // PUSH the INVOCATION into the output array w/ spread operator
+
+            // RETURN the first element of the array.
+  
+          
+  
+  
