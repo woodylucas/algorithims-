a875d93ea68cd8b22e3db8bc9f7b57b95fc620e3
woodylucas <wlucas0311@gmail.com>
2020-07-12 19:31:30 -0400

    start of promises

--- objects.js
+++ objects.js
@@ -6,3 +6,11 @@ const person = {
     tim: 28, 
     korey: 20,
 }
+
+let key = Object.keys(person)
+// console.log(keys) // ---> array of keys 
+let values = Object.values(person)
+ // console.log(values) //---> array of values
+ let entries = Object.entries(person)
+ // console.log(entries) --> nested array of both values and keys 
+ 
