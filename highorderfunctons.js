8ba7f3140630b30b9352a23803180a97ba403bad
woodylucas <wlucas0311@gmail.com>
2020-07-13 16:54:10 -0400

    solution solved--needs to be  refactored, redundant code

--- highorderfunctons.js
+++ highorderfunctons.js
@@ -233,3 +233,26 @@ function goodKeys(obj, callback) {
 // const startsWithBird = function(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
 // console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
 
+
+// Challenge 17
+function commutative(func1, func2, value) {
+    let output1 = func1(value); 
+    let output2 = func2(value)
+    let solution1 = func2(output1)
+    let solution2 = func1(output2)
+    if(solution1 === solution2) {
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
+  
+  // /*** Uncomment these to check your work! ***/
+  // const multBy3 = n => n * 3;
+  // const divBy4 = n => n / 4;
+  // const subtract5 = n => n - 5;
+  // console.log(commutative(multBy3, divBy4, 11)); // should log: true
+  // console.log(commutative(multBy3, subtract5, 10)); // should log: false
+  // console.log(commutative(divBy4, subtract5, 48)); // should log: false
+  
