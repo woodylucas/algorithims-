b316d1e58a31694e865ab55f9eee27e8485a06a4
woodylucas <wlucas0311@gmail.com>
2020-07-13 23:56:31 -0400

    final commit

--- highorderfunctons.js
+++ highorderfunctons.js
@@ -379,3 +379,22 @@ function myFunc(array, callback) {
 //   console.log(myFunc(numbers, isOdd)); // Output should be 1
 //   console.log(myFunc(evens, isOdd)); // Output should be -1
   
+// Challenge 24
+function myForEach(array, callback) {
+    let output;
+    for(const elem of array) {
+      output = callback(elem)
+    }
+    return output;
+  }
+  
+  let sum = 0;
+  
+  function addToSum(num) {
+    sum += num;
+  }
+  
+  // /*** Uncomment these to check your work! ***/
+  const nums = [1, 2, 3];
+  // myForEach(nums, addToSum);
+  // console.log(sum); // Should output 6
